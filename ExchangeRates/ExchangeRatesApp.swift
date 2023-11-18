9127eca70fabbb327f987ac71ae03d96573c6f71
Jean Camargo <jeancamargoreal@icloud.com>
2023-11-18 11:59:35 -0300

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Jean Camargo on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
